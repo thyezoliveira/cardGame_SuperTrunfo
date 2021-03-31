2bc0875024810c7a522c10fa1199d9238d94d00d
thyezoliveira <thyezoliveira@gmail.com>
2021-03-31 13:51:52 -0300

    migracao
    Migrei o projeto do codePen para o github.

---
+++ js/cartas.js
@@ -0,0 +1,218 @@
+export var cartas = [
+	{
+		nome: "Goku",
+		atributos: {
+			forca: 10,
+			defesa: 4,
+			magia: 8
+		},
+		url:"https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/05/09/goku2.jpg"
+	},
+	{
+		nome: "Yusuke Urameshi",
+		atributos: {
+			forca: 5,
+			defesa: 6,
+			magia: 9
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuxmFN4gQ4Xnfdz3dw0f5dSJA7fpGCW6Prsg&usqp=CAU"
+	},
+	{
+		nome: "Gojo Satoru",
+		atributos: {
+			forca: 6,
+			defesa: 4,
+			magia: 10
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6I9g4YXToGQPXg1UxImhccDCHPG7J2IEZDw&usqp=CAU"
+	},
+	{
+		nome: "Hulk",
+		atributos: {
+			forca: 10,
+			defesa: 10,
+			magia: 0
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSl5ZG8rK7rW5FV4i5JLDzSF2L3CyU73X1ZA&usqp=CAU"
+	},
+	{
+		nome: "Pack",
+		atributos: {
+			forca: 3,
+			defesa: 6,
+			magia: 10
+		},
+		url:"https://pm1.narvii.com/6437/9b4b50157761f661fe2a04177085ecfbfac012a9_hq.jpg"
+	},
+	{
+		nome: "Meowth",
+		atributos: {
+			forca: 1,
+			defesa: 1,
+			magia: 0
+		},
+		url:"https://i.pinimg.com/originals/88/c4/a2/88c4a25b66c336a6586f1d275070c5bd.jpg"
+	},
+	{
+		nome: "Yami Sukehiro",
+		atributos: {
+			forca: 4,
+			defesa: 4,
+			magia: 6
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjQTrBLq5KAuvEtz3zR8IjWFSjnAUS2WCIvQ&usqp=CAU"
+	},
+	{
+		nome: "Kibutsuji Muzan",
+		atributos: {
+			forca: 7,
+			defesa: 5,
+			magia: 9
+		},
+		url:"https://i1.sndcdn.com/artworks-000627186016-gh3l8f-t500x500.jpg"
+	},
+	{
+		nome: "Endeavor",
+		atributos: {
+			forca: 6,
+			defesa: 6,
+			magia: 7
+		},
+		url:"https://pbs.twimg.com/profile_images/1278506701293891584/wi5ESFVz.jpg"
+	},
+	{
+		nome: "Zenitsu",
+		atributos: {
+			forca: 2,
+			defesa: 1,
+			magia: 6
+		},
+		url:"https://i.pinimg.com/originals/ea/3b/1f/ea3b1fe57988b43b23f378fb6a03e2e8.jpg"
+	},
+	{
+		nome: "Arceus",
+		atributos: {
+			forca: 10,
+			defesa: 10,
+			magia: 10
+		},
+		url:"https://savepoint.com.br/games/wp-content/uploads/2017/02/Arceus.jpg"
+	},
+	{
+		nome: "Subaru Natsuki",
+		atributos: {
+			forca: 1,
+			defesa: 0,
+			magia: 0
+		},
+		url:"https://i1.sndcdn.com/artworks-000181641976-s2papw-t500x500.jpg"
+	},
+	{
+		nome: "Naofumi Iwatani",
+		atributos: {
+			forca: 1,
+			defesa: 10,
+			magia: 10
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5RQpoH8vE4O30bG5YFvMOTx9Mzjup060Pw&usqp=CAU"
+	},
+	{
+		nome: "Erza Scarlet",
+		atributos: {
+			forca: 5,
+			defesa: 5,
+			magia: 6
+		},
+		url:"https://i1.sndcdn.com/avatars-000461357652-e54lzp-t500x500.jpg"
+	},
+	{
+		nome: "Mereoleona Vermillion",
+		atributos: {
+			forca: 6,
+			defesa: 3,
+			magia: 7
+		},
+		url:"https://i.pinimg.com/736x/20/d9/4e/20d94e0f61a6918c179befe3d0d6bdac.jpg"
+	},
+	{
+		nome: "Echidna",
+		atributos: {
+			forca: 1,
+			defesa: 0,
+			magia: 10
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmatFzM7_4dQ6Kn9UUxlOnPdFd6ePUXrPP7w&usqp=CAU"
+	},
+	{
+		nome: "Patolino o Mago",
+		atributos: {
+			forca: 0,
+			defesa: 7,
+			magia: 10
+		},
+		url:"https://pbs.twimg.com/profile_images/1063084605669081088/jV8pJ1L7.jpg"
+	},
+	{
+		nome: "White Diamond",
+		atributos: {
+			forca: 8,
+			defesa: 8,
+			magia: 8
+		},
+		url:"https://i.pinimg.com/originals/b3/ec/ab/b3ecab82dc207e4029179a74202e0646.jpg"
+	},
+	{
+		nome: "Kyo Kusanagi",
+		atributos: {
+			forca: 5,
+			defesa: 4,
+			magia: 6
+		},
+		url:"https://cdn.myanimelist.net/r/360x360/images/characters/13/101150.jpg?s=73151e704a3c94db97d1ebde6a1b0359"
+	},
+	{
+		nome: "Goro Daimon",
+		atributos: {
+			forca: 7,
+			defesa: 5,
+			magia: 0
+		},
+		url:"https://64.media.tumblr.com/8220c8d735ea1c2bad18a2b1c1bc5980/tumblr_nevty09Pbh1qfxv5io3_500.png"
+	},
+	{
+		nome: "Athena Asamiya",
+		atributos: {
+			forca: 5,
+			defesa: 2,
+			magia: 10
+		},
+		url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_6U0iGCAl_uCuhfXAcfY9pipm17fzaeARkA&usqp=CAU"
+	},
+	{
+		nome: "Ichigo Kurosaki",
+		atributos: {
+			forca: 7,
+			defesa: 7,
+			magia: 8
+		},
+		url:"https://sites.google.com/site/sakiokazaki/_/rsrc/1337814336691/bleach/ichigo-kurosaki/hollowfied%20ichigo.jpg?height=300&width=400"
+	},
+	{
+		nome: "Merlin",
+		atributos: {
+			forca: 1,
+			defesa: 3,
+			magia: 10
+		},
+		url:"https://i.pinimg.com/originals/c5/11/21/c51121591a01d597b01ea450ad4117c9.jpg"
+	},
+	{
+		nome: "Amendo Bobo",
+		atributos: {
+			forca: 4,
+			defesa: 3,
+			magia: 7
+		},
+		url:"https://i1.sndcdn.com/artworks-000528513138-tcbhfi-t500x500.jpg"
+	}
+]
---
+++ js/game.js
@@ -0,0 +1,149 @@
+import {
+    cartas
+} from "./cartas.js"
+//---------------------------------
+var cartaMaquina;
+var cartaJogador;
+var pontosJogador = 0;
+var pontosMaquina = 0;
+var deck = [];
+var flipCardAudio = document.querySelector("#flipCard");
+var winAudio = document.querySelector("#win");
+var loseAudio = document.querySelector("#lose");
+for (var i = 0; i < cartas.length; i++) {
+    deck.push(cartas[i]);
+}
+
+console.log(deck);
+var state = 0;
+var flipCard1 = document.querySelector(".flip-card1");
+var flipCard2 = document.querySelector(".flip-card2");
+var flipCardInner1 = document.querySelector(".flip-card-inner1");
+var flipCardInner2 = document.querySelector(".flip-card-inner2");
+var sortear = document.querySelector(".sortear");
+
+sortear.addEventListener("click", sortearCarta);
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * deck.length);
+    cartaMaquina = deck[numeroCartaMaquina];
+
+    var numeroCartajogador = parseInt(Math.random() * deck.length);
+    while (numeroCartajogador == numeroCartaMaquina) {
+        numeroCartajogador = parseInt(Math.random() * deck.length);
+    }
+    cartaJogador = deck[numeroCartajogador];
+
+    document.querySelector('.sortear').style.display = "none";
+
+    updateData();
+    exibirOpcoes();
+}
+
+function updateData() {
+    var nomeCard1 = document.querySelector(".frontCardTitulo1");
+    var nomeCard2 = document.querySelector(".frontCardTitulo2");
+    var imgCard1 = document.querySelector(".frontCardImage1");
+    var imgCard2 = document.querySelector(".frontCardImage2");
+    var forca1 = document.querySelector(".forca1");
+    var defesa1 = document.querySelector(".defesa1");
+    var magia1 = document.querySelector(".magia1");
+    var forca2 = document.querySelector(".forca2");
+    var defesa2 = document.querySelector(".defesa2");
+    var magia2 = document.querySelector(".magia2");
+
+    nomeCard1.innerText = cartaJogador.nome;
+    imgCard1.src = cartaJogador.url;
+    forca1.innerText = cartaJogador.atributos.forca;
+    defesa1.innerText = cartaJogador.atributos.defesa;
+    magia1.innerText = cartaJogador.atributos.magia;
+
+    nomeCard2.innerText = cartaMaquina.nome;
+    imgCard2.src = cartaMaquina.url;
+    forca2.innerText = cartaMaquina.atributos.forca;
+    defesa2.innerText = cartaMaquina.atributos.defesa;
+    magia2.innerText = cartaMaquina.atributos.magia;
+
+    flipCard1.classList.add("pulse");
+    flipCardInner1.style.transform = "rotateY(180deg)";
+    flipCardAudio.play();
+}
+
+function exibirOpcoes() {
+    var opcoes = document.querySelector(".options");
+    var opcoesTexto = "";
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += `<button id="btn${atributo}" class="btnatributo">${atributo}</button>`;
+    }
+
+    var template = `<p>Que atributo você deseja comparar?</p><br>
+	${opcoesTexto}`;
+    opcoes.innerHTML = template;
+    adicionarEventos();
+}
+
+function adicionarEventos() {
+    var btns = document.querySelectorAll(".btnatributo");
+    btns.forEach(button => {
+        button.addEventListener("click", () => {
+            obtemAtributoSelecionado(button.innerText);
+        })
+    })
+}
+
+function obtemAtributoSelecionado(atributo) {
+    jogar(atributo);
+}
+
+function jogar(atributoSelecionado) {
+    var placarJogador = document.querySelector("#placarJogador");
+    var placarMaquina = document.querySelector("#placarMaquina");
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        mostraResultado("Você venceu!");
+        pontosJogador++;
+        placarJogador.innerText = pontosJogador;
+        winAudio.play();
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        mostraResultado("Você perdeu!");
+        pontosMaquina++;
+        placarMaquina.innerText = pontosMaquina;
+        loseAudio.play();
+    } else {
+        mostraResultado("Empatou!");
+    }
+
+    if (state == 0) {
+        flipCard2.classList.add("pulse");
+        flipCardInner2.style.transform = "rotateY(180deg)";
+        flipCardAudio.play();
+        state = 1;
+    }
+}
+
+function mostraResultado(msg) {
+    var resultado = document.querySelector(".comandos");
+    var template = `<h1>${msg}</h1><br><button class="jogarNovamente">Jogar Novamente</button>`;
+    resultado.innerHTML = template;
+    var jogarNovamente = document.querySelector(".jogarNovamente");
+    jogarNovamente.addEventListener("click", reiniciar);
+}
+
+function reiniciar() {
+    flipCardInner1.style.transform = "rotateY(0deg)";
+    flipCardInner2.style.transform = "rotateY(0deg)";
+    flipCard1.classList.remove("pulse");
+    flipCard2.classList.remove("pulse");
+    state = 0;
+
+    var comandos = document.querySelector(".comandos");
+    var template = `<button class="sortear">Sortear carta</button>
+			<div class="options"></div>
+			`;
+
+    setTimeout(() => {
+        comandos.innerHTML = template;
+        var sortearbtn = document.querySelector(".sortear");
+        sortearbtn.addEventListener("click", sortearCarta);
+    }, 500);
+}
